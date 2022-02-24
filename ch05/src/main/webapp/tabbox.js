1dc53f2c98b0cac1fe349a6434ce35d85367e4d9
shk4548 <shk4548@gmail.com>
2022-02-24 11:57:20 +0900

    jquery 선택자

--- ch05/src/main/webapp/tabbox.js
+++ ch05/src/main/webapp/tabbox.js
@@ -1,14 +1,15 @@
 var tabBox = {
 	onWindowLoad: function(){
-		console.log(this);
 		var divTabBox = document.getElementsByClassName("tab-box")[0];
 		var ul = divTabBox.childNodes[1];
 		
 		var liTabs = ul.getElementsByTagName("li");
 		for(var i = 0; i < liTabs.length; i++){
 			liTabs[i].addEventListener("click",this._onTabClicked);
+			console.log(this + " 아아아");
+
 		}
-		
+
 	},
 	_onTabClicked: function(){
 		//unselect
@@ -16,9 +17,15 @@ var tabBox = {
 		(lisSelected.length === 1) && (lisSelected[0].className=""); 
 		
 		//select
+		console.log(this + " 셀렉티드");
+
 		this.className = 'selected';
 	},
 	init: function(){
 		window.addEventListener('load', this.onWindowLoad.bind(this)); // this -> tabbox 
+				console.log("--------");
+				console.log(this);
+				console.log("--------");
+
 	}
 }
